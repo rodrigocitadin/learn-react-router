371a2910464eb145d5799503cd46cbcbf95bbe12
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-22 18:38:53 -0300

    contact loader

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Root, { loader as rootLoader, action as rootAction } from './routes/root'
 import ErrorPage from './error-page'
-import Contact from './routes/contact'
+import Contact, { loader as contactLoader } from './routes/contact'
 
 const router = createBrowserRouter([
   {
@@ -16,7 +16,8 @@ const router = createBrowserRouter([
     children: [
       {
         path: "contacts/:contactId",
-        element: <Contact />
+        element: <Contact />,
+        loader: contactLoader
       }
     ]
   },
