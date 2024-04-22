b9c888a13e6ce7d8a394762a1400a6ec4992f0a0
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-22 12:23:09 -0300

    change default route to Root route

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Root from './routes/root'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello</div>
+    element: <Root />
   }
 ])
 
