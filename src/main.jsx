b42573520e3f0fad1141dcf3db09b03258f774e1
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-22 12:33:05 -0300

    change to a children route and use outlet to render in the same page

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,20 @@ import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Root from './routes/root'
 import ErrorPage from './error-page'
+import Contact from './routes/contact'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <ErrorPage />
-  }
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />
+      }
+    ]
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
