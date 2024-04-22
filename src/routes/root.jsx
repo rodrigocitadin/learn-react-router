ae8351334a252cd476e68e23cd58eb570487bda3
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-22 18:52:48 -0300

    create new contacts

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Form, Link, Outlet, useLoaderData } from "react-router-dom"
+import { redirect, Form, Link, Outlet, useLoaderData } from "react-router-dom"
 import { getContacts, createContact } from "../contacts";
 
 export async function loader() {
@@ -8,7 +8,7 @@ export async function loader() {
 
 export async function action() {
   const contact = await createContact();
-  return { contact }
+  return redirect(`/contacts/${contact.id}/edit`)
 }
 
 export default function Root() {
