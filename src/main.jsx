02ce374cd8aff00827b363dee2129066c542933c
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-24 07:37:26 -0300

    add and remove fav without navigation

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Root, { loader as rootLoader, action as rootAction } from './routes/root'
 import ErrorPage from './error-page'
-import Contact, { loader as contactLoader } from './routes/contact'
+import Contact, { loader as contactLoader, action as contactAction } from './routes/contact'
 import EditContact, { action as editAction } from './routes/edit'
 import { action as destroyAction } from "./routes/destroy"
 import Index from './routes'
@@ -20,7 +20,8 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
-        loader: contactLoader
+        loader: contactLoader,
+        action: contactAction
       },
       {
         path: "contacts/:contactId/edit",
